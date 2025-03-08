32a3659b9fc7655a1476bb8584ee762b84b4398f
Rick Markowitz <rick@fanslove.com>
2025-03-08 12:36:11 -0800

    package.json for combined repositories

--- Server/server.ts
+++ Server/server.ts
@@ -7,24 +7,18 @@ const server: FastifyInstance = Fastify({
     logger: true,
 });
 
-// Register WebSocket plugin
 server.register(fastifyWebsocket);
 server.register(async function (fastify) {
     fastify.get('/socket', { websocket: true }, (socket: WebSocket) => {
         socket.on('update', (message: Buffer) => {
-            // message.toString() === 'hi from client'
             socket.send(message);
         });
-        socket.send('Hello from server');
     });
 });
 
-// WebSocket route
-
-// Run the server!
 try {
-    await server.listen({ port: 3000, host: '0.0.0.0' });
-    console.log('Server is running on port 3000');
+    await server.listen({ port: 2761, host: '0.0.0.0' });
+    console.log('Server is running on port 2761');
 } catch (err) {
     server.log.error(err);
     process.exit(1);
