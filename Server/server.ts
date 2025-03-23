4bb839db69705a150704cbfdd2afd900fd467647
Rick Markowitz <rick@fanslove.com>
2025-03-22 19:13:49 -0700

    fastify => hono

--- Server/server.ts
+++ Server/server.ts
@@ -1,25 +1,33 @@
-import Fastify from 'fastify';
-import { FastifyInstance } from 'fastify';
-import fastifyWebsocket from '@fastify/websocket';
-import { WebSocket } from 'ws';
+import { Hono } from 'hono';
+import { upgradeWebSocket } from 'hono/cloudflare-workers';
 
-const server: FastifyInstance = Fastify({
-    logger: true,
-});
+const app = new Hono();
 
-server.register(fastifyWebsocket);
-server.register(async function (fastify) {
-    fastify.get('/socket', { websocket: true }, (socket: WebSocket) => {
-        socket.on('update', (message: Buffer) => {
-            socket.send(message);
-        });
+app.get('/', (c) => {
+    return c.json({
+        ok: true,
+        message: 'Hello World!',
     });
 });
 
-try {
-    await server.listen({ port: 2761, host: '0.0.0.0' });
-    console.log('Server is running on port 2761');
-} catch (err) {
-    server.log.error(err);
-    process.exit(1);
-}
+app.get(
+    '/update',
+    upgradeWebSocket((c) => {
+        return {
+            onMessage(event, ws) {
+                console.log(`Message from client: ${event.data}`);
+                ws.send('Hello from server!');
+            },
+            onClose: () => {
+                console.log('Connection closed');
+            },
+        };
+    })
+);
+
+console.log('Server is running on port 8787');
+
+// Add this export
+export default {
+    fetch: app.fetch,
+};
